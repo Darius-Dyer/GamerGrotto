7b104b8e58b09b0bdf254ee48844c35e9af1ad13
Darius-Dyer <dyasar256@gmail.com>
2025-09-13 18:20:14 -0400

    Before Adding Icons

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -3,29 +3,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import SettingScreen from "../screens/SettingScreen";
-import DisplayScreen from "../screens/DisplayScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import ShelfScreen from "../screens/ShelfScreen";
 import RegistrationScreen from "../screens/RegistrationScreen";
+import * as Icons from "react-native-vector-icons/FontAwesome6";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-const NativeStackNavigation = () => {
-  <Stack.Navigator>
-    <Stack.Screen name="Home" component={HomeScreen} />
-    <Stack.Screen name="Registration" component={RegistrationScreen} />
-    <Stack.Screen name="Profile" component={ProfileScreen} />
-  </Stack.Navigator>;
-};
-
 const BottomTabNavigation = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{ tabBarLabel: Icons.FontAwesome6IconButton.house }}
+    />
     <Tab.Screen name="Settings" component={SettingScreen} />
     <Tab.Screen name="Profile" component={ProfileScreen} />
     <Tab.Screen name="Shelf" component={ShelfScreen} />
-    <Tab.Screen name="Display" component={DisplayScreen} />
   </Tab.Navigator>
 );
 
--- screens/ShelfScreen.js
+++ screens/ShelfScreen.js
@@ -1,10 +1,37 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const ShelfScreen = () => {
   return (
-    <View style={{ flex: 1, centerItems: "center", justifyContent: "center" }}>
-      <Text>Shelf Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Shelf Screen</Text>
+      <Text style={styles.mainText}>
+        On this screen you may add any game from the database and, view all of
+        th games from your personal shelf.
+      </Text>
     </View>
   );
 };
+const styles = createStyleSheet({
+  container: {
+    flex: 1,
+    centerItems: "center",
+    justifyContent: "center",
+  },
+  mainView: {
+    backgroundColor: "#ADD8E6",
+    flex: 1,
+    alignItems: "center",
+  },
+  titleText: {
+    textAlign: "center",
+    margin: 20,
+    fontSize: 30,
+  },
+  mainText: {
+    textAlign: "center",
+    margin: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
 export default ShelfScreen;
