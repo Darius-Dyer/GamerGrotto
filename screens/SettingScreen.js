3523536c85fa6d97a6ceb3ec98f0303e9f5f75d3
Darius-Dyer <dyasar256@gmail.com>
2025-11-29 18:52:02 -0500

    fix: Added Styling to Setting Screen.

--- screens/SettingScreen.js
+++ screens/SettingScreen.js
@@ -2,8 +2,17 @@ import { View, Text } from "react-native";
 
 const SettingScreen = () => {
   return (
-    <View style={{ flex: 1, centerItems: "center", justifyContent: "center" }}>
-      <Text>Settings Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        centerItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#1e1e1e",
+      }}
+    >
+      <Text style={{ textAlign: "center", fontSize: 30, color: "#fff" }}>
+        Settings Screen
+      </Text>
     </View>
   );
 };
